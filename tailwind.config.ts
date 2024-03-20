38624583d5546d97a7679c9ed505b5d0ac79cf7a
TaeWooKim-SCH <zop1234@hanmail.net>
2024-03-20 21:30:11 +0900

    setting: 폰트, css 초기화

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,11 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      "purple": "#756AB6",
+      "light-purple": "#9D95CA",
+      "gray": "#848290"
+    }
   },
   plugins: [],
 };
