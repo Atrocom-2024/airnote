785f71840f4a856525f4375fef41dd1f60a907a1
TaeWooKim-SCH <zop1234@hanmail.net>
2024-03-27 18:05:51 +0900

    feat: 홈 화면에 사이드 창 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ export default function Home() {
   return (
     <Layout>
       <HomeMapSection />
+      <div className="absolute top-[10vh] left-5 w-[400px] h-[75vh] bg-white shadow-lg z-50">
+        안녕
+      </div>
     </Layout>
   );
 }
