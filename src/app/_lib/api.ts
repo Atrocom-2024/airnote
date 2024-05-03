c67b9d6c81e884aed14cebfafbe5f1ab6ca6886c
TaeWooKim-SCH <zop1234@hanmail.net>
2024-05-03 17:14:16 +0900

    fix: 관리자 로그인 api 모듈화

--- src/app/_lib/api.ts
+++ src/app/_lib/api.ts
@@ -1,3 +1,7 @@
+import { encrypt } from "@/utils/modules";
+import { throttle } from "lodash";
+import { SubmitHandler } from "react-hook-form";
+
 // 위치에 따른 리뷰 데이터를 가져오는 함수
 export const getReviews = async (lat: string, lng: string) => {
   const domain = process.env.NEXT_PUBLIC_DOMAIN;
@@ -32,12 +36,50 @@ export const getSearchResults = async (keyword: string) => {
   return res.json();
 };
 
+// 관리자 로그인 요청
+export const postLogin: SubmitHandler<FormInputs> = throttle(async (data) => {
+  if (!data.id) {
+    return alert('아이디를 입력해주세요.');
+  } else if (!data.password) {
+    return alert('비밀번호를 입력해주세요.');
+  }
+  try {
+    const domain = process.env.NEXT_PUBLIC_DOMAIN;
+    const res = await fetch(`${domain}/api/admin/login`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        id: encrypt(data.id, process.env.NEXT_PUBLIC_AES_ID_SECRET_KEY),
+        password: encrypt(data.password, process.env.NEXT_PUBLIC_AES_PW_SECRET_KEY)
+      })
+    });
+    if (!res.ok) {
+      return alert('아이디 또는 비밀번호가 일치하지 않습니다.');
+    } else {
+      window.location.reload();
+    }
+  } catch (err) {
+    console.error('로그인 실패', err);
+    alert('로그인에 실패하였습니다. 잠시 후 다시 시도해주세요.');
+  }
+}, 2000);
+
 // 관리자 로그아웃 요청
-export const postLogout = async () => {
+export const postLogout = throttle(async () => {
   const domain = process.env.NEXT_PUBLIC_DOMAIN;
   const res = await fetch(`${domain}/api/admin/logout`, { method: 'POST' });
   if (!res.ok) {
     throw new Error('Failed to logout');
   }
   return res.status;
+}, 2000);
+
+
+
+
+
+
+interface FormInputs {
+  id: string;
+  password: string;
 }
--- src/app/admin/_components/AdminLoginForm.tsx
+++ src/app/admin/_components/AdminLoginForm.tsx
@@ -1,49 +1,19 @@
 'use client'
 
-import { useRouter } from "next/navigation";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { FaUser } from "react-icons/fa6";
 import { RiLock2Fill } from "react-icons/ri";
-import { throttle } from "lodash";
 
-import { encrypt } from "@/utils/modules";
+import { postLogin } from "@/app/_lib/api";
 import LoadingUI from "@/app/_components/LoadingUI";
 
 export default function AdminLoginForm() {
-  const router = useRouter();
   const { register, handleSubmit, formState: { isSubmitting } } = useForm<FormInputs>({
     defaultValues: { id: '', password: ''}
   });
-
-  const onSubmit: SubmitHandler<FormInputs> = throttle(async (data) => {
-    if (!data.id) {
-      return alert('아이디를 입력해주세요.');
-    } else if (!data.password) {
-      return alert('비밀번호를 입력해주세요.');
-    }
-    try {
-      const domain = process.env.NEXT_PUBLIC_DOMAIN;
-      const res = await fetch(`${domain}/api/admin/login`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          id: encrypt(data.id, process.env.NEXT_PUBLIC_AES_ID_SECRET_KEY),
-          password: encrypt(data.password, process.env.NEXT_PUBLIC_AES_PW_SECRET_KEY)
-        })
-      });
-      if (!res.ok) {
-        return alert('아이디 또는 비밀번호가 일치하지 않습니다.');
-      } else {
-        router.push('/admin/home');
-      }
-    } catch (err) {
-      console.error('로그인 실패', err);
-      alert('로그인에 실패하였습니다. 잠시 후 다시 시도해주세요.');
-    }
-  }, 2000);
   
   return (
-    <form className="grid grid-cols-1" onSubmit={handleSubmit(onSubmit)}>
+    <form className="grid grid-cols-1" onSubmit={handleSubmit(postLogin)}>
       {isSubmitting && <LoadingUI />}
       <section className="mb-3 flex justify-center items-center">
         <div className="mr-2">
