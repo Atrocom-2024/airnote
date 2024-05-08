13306f144213c3d04e7c9ae8c54c05c218e6cd8d
TaeWooKim-SCH <zop1234@hanmail.net>
2024-05-08 20:46:05 +0900

    feat: 관리자 페이지 관련 데이터 요청 시 권한 확인

--- src/pages/api/admin/users.ts
+++ src/pages/api/admin/users.ts
@@ -2,8 +2,22 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Db } from "mongodb";
 
 import { connectDB } from "@/utils/database";
+import { verifyToken } from "@/utils/jwtUtils";
 
+// TODO: 관리자 권한 확인하기
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // 토큰 확인
+  const accessToken = req.cookies.accessToken;
+  if (accessToken) {
+    const { valid } = await verifyToken(accessToken);
+    if (!valid) {
+      return res.status(401).send('권한 없음');
+    }
+  } else {
+    return res.status(401).send('비로그인 상태');
+  }
+  
+  // 요청 처리
   switch (req.method) {
     case 'GET':
       const { username } = req.query;
