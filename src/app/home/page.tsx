cafd0a17baab20898acd1314b3e31aead362750a
TaeWooKim-SCH <zop1234@hanmail.net>
2024-06-23 15:15:33 +0900

    test: 에러 수정 테스트

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
 async function getTopReviews() {
   const domain = process.env.NEXT_PUBLIC_DOMAIN;
   const res = await fetch(`${domain}/api/reviews/top`, { cache: 'no-store' });
-
+  
   if (!res.ok) {
     throw new Error('Failed to fetch data');
   }
