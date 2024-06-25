9a9d000d3843649356e82ac6d631833220cc9ac8
TaeWooKim-SCH <zop1234@hanmail.net>
2024-06-25 15:22:35 +0900

    feat: 랜딩 페이지 요소 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,29 +8,77 @@ export default async function Home() {
       <section className="py-4 md:px-4 md:py-8">
         <Image className="w-[50px] md:w-[80px]" src="/landing-logo.svg" width={100} height={0} alt="로고" />
       </section>
-      <section className="flex justify-center items-center">
-        <article className="text-white md:mr-20">
-          <div className="text-xl font-bold mb-2 md:text-2xl lg:text-3xl">남녀노소 누구든지</div>
-          <div className="text-xl font-bold mb-5 md:text-2xl lg:text-3xl">공간에 대한 기록을 남길 수 있어요</div>
-          <div className="text-sm mb-10 md:mb-16 md:text-base lg:text-lg">여러분들의 소중한 기록이 모든 사람에게 도움이 될 수 있어요</div>
-          <Image
-            className="w-full mx-auto mb-10 shadow-2xl rounded-md md:hidden"
-            src="/landing-bg-img.png"
-            width={500}
-            height={0}
-            alt="예시 이미지"
-          />
-          <Link className="bg-white text-default text-sm font-bold rounded-md px-6 py-2 md:text-xl" href="/home">시작하기</Link>
-        </article>
-        <article className="hidden md:block">
-          <Image
-            className="shadow-2xl rounded-md md:w-[400px] lg:w-[800px]"
-            src="/landing-bg-img.png"
-            width={800}
-            height={0}
-            alt="예시 이미지"
-          />
-        </article>
+      <section className="min-h-[60vh] px-4 overflow-y-scroll" id="landing-container">
+        <section className="h-full flex justify-center items-center">
+          <article className="text-white md:mr-20">
+            <div className="text-xl font-bold mb-2 md:text-2xl lg:text-3xl">남녀노소 누구든지</div>
+            <div className="text-xl font-bold mb-5 md:text-2xl lg:text-3xl">공간에 대한 기록을 남길 수 있어요</div>
+            <div className="text-sm mb-10 md:mb-16 md:text-base lg:text-lg">여러분들의 소중한 기록이 모든 사람에게 도움이 될 수 있어요</div>
+            <Image
+              className="w-full mx-auto mb-10 shadow-2xl rounded-md md:hidden"
+              src="/landing-bg-img-1.png"
+              width={500}
+              height={0}
+              alt="예시 이미지"
+            />
+            <Link className="bg-white text-default text-sm font-bold rounded-md px-6 py-2 md:text-xl" href="/home">시작하기</Link>
+          </article>
+          <article className="hidden md:block">
+            <Image
+              className="shadow-2xl rounded-md md:w-[400px] lg:w-[800px]"
+              src="/landing-bg-img-1.png"
+              width={800}
+              height={0}
+              alt="예시 이미지"
+            />
+          </article>
+        </section>
+        <section className="h-full flex justify-center items-center">
+          <article className="hidden md:block">
+            <Image
+              className="shadow-2xl rounded-md md:w-[400px] lg:w-[800px]"
+              src="/landing-bg-img-2.png"
+              width={800}
+              height={0}
+              alt="예시 이미지"
+            />
+          </article>
+          <article className="text-white md:ml-20">
+            <div className="text-xl font-bold mb-5 md:text-2xl lg:text-3xl">공간 기록 구경하기</div>
+            <div className="text-sm mb-10 md:mb-16 md:text-base lg:text-lg">지도의 마커를 클릭하면<br />다양한 사람들이 작성한 공간 기록을 구경할 수 있어요</div>
+            <Image
+              className="w-full mx-auto mb-10 shadow-2xl rounded-md md:hidden"
+              src="/landing-bg-img-2.png"
+              width={500}
+              height={0}
+              alt="예시 이미지"
+            />
+            {/* <Link className="bg-white text-default text-sm font-bold rounded-md px-6 py-2 md:text-xl" href="/home">시작하기</Link> */}
+          </article>
+        </section>
+        <section className="h-full flex justify-center items-center">
+          <article className="text-white md:mr-20">
+            <div className="text-xl font-bold mb-5 md:text-2xl lg:text-3xl">공간 기록 작성하기</div>
+            <div className="text-sm mb-10 md:mb-16 md:text-base lg:text-lg">기록작성 페이지에서 좋았던 경험, 안 좋았던 경험 등<br />자신이 경험했던 공간에 대해 자유롭게 기록을 남길 수 있어요<br />남들에게 편하게 할 수 없던 다양한 경험들을 공유해보세요</div>
+            <Image
+              className="w-full mx-auto mb-10 shadow-2xl rounded-md md:hidden"
+              src="/landing-bg-img-3.png"
+              width={500}
+              height={0}
+              alt="예시 이미지"
+            />
+            <Link className="bg-white text-default text-sm font-bold rounded-md px-6 py-2 md:text-xl" href="/home">시작하기</Link>
+          </article>
+          <article className="hidden md:block">
+            <Image
+              className="shadow-2xl rounded-md md:w-[400px] lg:w-[800px]"
+              src="/landing-bg-img-3.png"
+              width={800}
+              height={0}
+              alt="예시 이미지"
+            />
+          </article>
+        </section>
       </section>
       <section className="text-lg text-default font-bold flex flex-col items-center md:text-2xl">
         <div>더 알아보려면 아래로 스크롤하세요</div>
