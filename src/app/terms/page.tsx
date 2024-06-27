7a0b82eaf3b5557000cc7c0f89a790c0fa16a8c1
TaeWooKim-SCH <zop1234@hanmail.net>
2024-06-27 19:07:54 +0900

    fix: 약관 동의 페이지 스타일 수정

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -7,13 +7,13 @@ import TermsDetail from "./_components/TermsDetail";
 export default function TermsPage() {
   return (
     <Layout>
-      <main className="w-full mx-auto py-10 md:w-[600px]">
+      <main className="w-full px-5 pt-10 md:mx-auto md:w-[600px]">
         <Title>공기수첩 이용약관</Title>
         <article className="mt-10">
           <TermsDetail />
         </article>
         <Link
-          className="block bg-default w-full py-2 mt-10 text-white text-center text-lg font-bold rounded-md"
+          className="block bg-default w-full py-2 mt-5 text-white text-center text-lg font-bold rounded-md"
           href="/home"
         >돌아가기</Link>
       </main>
