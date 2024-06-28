b5279c3b0931740e6b7744af7c0b5da8733cf716
TaeWooKim-SCH <zop1234@hanmail.net>
2024-06-28 17:19:31 +0900

    feat: 건물 정보 오버레이 구현중

--- src/app/_components/map/MapSection.tsx
+++ src/app/_components/map/MapSection.tsx
@@ -28,6 +28,36 @@ export default function MapSection() {
   const markerClickHandler = (lat: number, lng: number, address: string) => {
     router.push(`/home?sidebar=true&lat=${lat}&lng=${lng}&address=${encodeURIComponent(address)}`);
   }
+
+  const getAddress = async (_: any, mouseEvent: any) => {
+    const url = 'https://dapi.kakao.com/v2/local/geo/coord2address.json';
+    const headerAuthorization = `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}`
+    const latlng = mouseEvent.latLng;
+    const x = latlng.getLng();
+    const y = latlng.getLat();
+    const res = await fetch(`${url}?x=${x}&y=${y}`, {
+      headers: {
+        'Authorization': headerAuthorization
+      }
+    });
+    const json = await res.json();
+    const address = await json.documents[0].address.address_name;
+    const buildingInfo = await getLocationInfo(address);
+    console.log(buildingInfo);
+  }
+
+  const getLocationInfo = async (address: string) => {
+    const url = 'https://dapi.kakao.com/v2/local/search/address';
+    const headerAuthorization = `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}`
+    const res = await fetch(`${url}?query=${address}`, {
+      headers: {
+        'Authorization': headerAuthorization
+      }
+    });
+    const json = await res.json();
+    const buildingInfo = json.documents[0].road_address;
+    return buildingInfo;
+  }
   
   useEffect(() => {
     paramLat && paramLng ? setMapLoc({ lat: Number(paramLat), lng: Number(paramLng) }) : getAsyncLocationHandler();
@@ -39,6 +69,7 @@ export default function MapSection() {
       level={9}
       style={{ width: "100%", height: "84vh" }}
       isPanto={true}
+      onClick={getAddress}
     >
       {loading && <PartLoadingUI />}
       <MapComponent setMarkerInfo={setMarkerInfo} />
