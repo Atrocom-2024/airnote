14021117eca6a7f71aa715fec7bd5a61e6b1acf7
TaeWooKim-SCH <zop1234@hanmail.net>
2024-07-15 15:22:44 +0900

    feat: 에디터 스타일

--- src/app/_components/QuillDynamicWrapper.tsx
+++ src/app/_components/QuillDynamicWrapper.tsx
@@ -4,17 +4,60 @@ import dynamic from "next/dynamic";
 import ReactQuill, { ReactQuillProps } from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 
-import PartLoadingUI from "./PartLoadingUI";
+import LoadingUI from "./LoadingUI";
+import { useEffect, useRef } from "react";
+
+const modules = {
+  toolbar: [
+    [{ font: [] }],
+    [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image"],
+    ["clean"],
+  ],
+};
+
+const formats = [
+  "font",
+  "size",
+  "header",
+  "color",
+  "background",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+];
 
 const QuillDynamicWrapper = dynamic(
   async () => {
     const { default: QuillComponent } = await import('react-quill');
     const Quill = ({ forwardedRef, ...props }: ForwardedQuillComponent) => (
-      <QuillComponent ref={forwardedRef} {...props} />
+      <QuillComponent
+        ref={forwardedRef}
+        style={{borderRadius: '10px'}}
+        modules={modules}
+        formats={formats}
+        {...props}
+      />
     );
     return Quill;
   },
-  { loading: () => <PartLoadingUI />, ssr: false }
+  { loading: () => <LoadingUI className="mt-10" />, ssr: false }
 );
 
 interface ForwardedQuillComponent extends ReactQuillProps {
@@ -22,3 +65,4 @@ interface ForwardedQuillComponent extends ReactQuillProps {
 }
 
 export default QuillDynamicWrapper;
+
--- src/app/knowledge/add/_components/KnowledgeAddFormQuill.tsx
+++ src/app/knowledge/add/_components/KnowledgeAddFormQuill.tsx
@@ -1,14 +1,16 @@
 'use client';
 
-import QuillDynamicWrapper from "@/app/_components/QuilldynamicWrapper";
 import { useRef } from "react";
 import ReactQuill from "react-quill";
+
+import QuillDynamicWrapper from "@/app/_components/QuilldynamicWrapper";
 
 export default function KnowledgeAddFormQuill() {
   const quillRef = useRef<ReactQuill>(null);
 
   return (
     <QuillDynamicWrapper
+      className="mt-3 h-[300px]"
       forwardedRef={quillRef}
       theme="snow"
     />
