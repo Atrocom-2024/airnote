06f03f9dda7f4349d1bb12c1ba76245bee291d17
TaeWooKim-SCH <zop1234@hanmail.net>
2024-07-15 15:34:43 +0900

    fix: 툴바 오류 수정

--- src/app/_components/QuillDynamicWrapper.tsx
+++ src/app/_components/QuillDynamicWrapper.tsx
@@ -5,43 +5,6 @@ import ReactQuill, { ReactQuillProps } from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 
 import LoadingUI from "./LoadingUI";
-import { useEffect, useRef } from "react";
-
-const modules = {
-  toolbar: [
-    [{ font: [] }],
-    [{ size: ["small", false, "large", "huge"] }], // custom dropdown
-    [{ header: [1, 2, 3, 4, 5, 6, false] }],
-    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-    ["bold", "italic", "underline", "strike", "blockquote"],
-    [
-      { list: "ordered" },
-      { list: "bullet" },
-      { indent: "-1" },
-      { indent: "+1" },
-    ],
-    ["link", "image"],
-    ["clean"],
-  ],
-};
-
-const formats = [
-  "font",
-  "size",
-  "header",
-  "color",
-  "background",
-  "bold",
-  "italic",
-  "underline",
-  "strike",
-  "blockquote",
-  "list",
-  "bullet",
-  "indent",
-  "link",
-  "image",
-];
 
 const QuillDynamicWrapper = dynamic(
   async () => {
@@ -50,8 +13,6 @@ const QuillDynamicWrapper = dynamic(
       <QuillComponent
         ref={forwardedRef}
         style={{borderRadius: '10px'}}
-        modules={modules}
-        formats={formats}
         {...props}
       />
     );
--- src/app/knowledge/add/_components/KnowledgeAddFormQuill.tsx
+++ src/app/knowledge/add/_components/KnowledgeAddFormQuill.tsx
@@ -1,17 +1,64 @@
 'use client';
 
-import { useRef } from "react";
+import { useMemo, useRef, useState } from "react";
 import ReactQuill from "react-quill";
 
 import QuillDynamicWrapper from "@/app/_components/QuilldynamicWrapper";
 
+const formats = [
+  'font',
+  'header',
+  'bold',
+  'italic',
+  'underline',
+  'strike',
+  'blockquote',
+  'list',
+  'bullet',
+  'indent',
+  'link',
+  'align',
+  'color',
+  'background',
+  'size',
+  'h1',
+];
+
 export default function KnowledgeAddFormQuill() {
+  const [ values, setValues ] = useState('');
   const quillRef = useRef<ReactQuill>(null);
+
+  const modules = useMemo(() => {
+    return {
+      toolbar: {
+        container: [
+          [{ size: ['small', false, 'large', 'huge'] }],
+          [{ align: [] }],
+          ['bold', 'italic', 'underline', 'strike'],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          [
+            {
+              color: [],
+            },
+            { background: [] },
+          ],
+        ],
+      },
+    };
+  }, []);
+
+  const editorChangeHandler = (e: any) => {
+    setValues(e);
+  }
 
   return (
     <QuillDynamicWrapper
       className="mt-3 h-[300px]"
       forwardedRef={quillRef}
+      modules={modules}
+      formats={formats}
+      value={values}
+      onChange={editorChangeHandler}
       theme="snow"
     />
   );
