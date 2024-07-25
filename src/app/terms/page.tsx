0b5aabd4ee06e46c8747c6598396b7dfc4cab64b
TaeWooKim-SCH <zop1234@hanmail.net>
2024-07-25 16:17:21 +0900

    feat: 개인정보처리방침 추가

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -1,16 +1,25 @@
 import Layout from "../_components/layouts/Layout";
 import Title from "../_components/Title";
 import TermsDetail from "./_components/TermsDetail";
+import PrivacyDetail from "./_components/PrivacyDetail";
 import BackBtn from "./_components/BackBtn";
 
 export default function TermsPage() {
   return (
     <Layout>
       <main className="w-full px-5 pt-10 pb-10 md:mx-auto md:w-[600px]">
-        <Title>공기수첩 이용약관</Title>
-        <article className="mt-10">
-          <TermsDetail />
-        </article>
+        <section>
+          <Title>공기수첩 이용약관</Title>
+          <article className="mt-5">
+            <TermsDetail />
+          </article>
+        </section>
+        <section className="mt-10">
+          <Title>공기수첩 개인정보처리방침</Title>
+          <article className="mt-5">
+            <PrivacyDetail />
+          </article>
+        </section>
         <BackBtn />
       </main>
     </Layout>
