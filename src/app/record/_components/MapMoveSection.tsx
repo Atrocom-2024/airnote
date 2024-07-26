ba657e11a57bdea77965cb7bb9caadaf50117283
TaeWooKim-SCH <zop1234@hanmail.net>
2024-07-26 13:12:32 +0900

    feat: 지도 위치 이동 및 모바일 스타일

---
+++ src/app/record/_components/MapMoveSection.tsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { LuArrowUpRightSquare } from "react-icons/lu";
+
+import { cityNameInfo } from "@/app/_lib/data";
+import { getBuildingInfo } from "@/app/_lib/api";
+import { useMapLocation } from "@/app/_lib/store";
+
+export default function MapMoveSection({ setIsMap }: PropsType) {
+  const { setMapLoc } = useMapLocation();
+  const [ city, setCity ] = useState<string>('시/도 선택');
+  const [ regionName, setRegionName ] = useState<string>('시/군/구 선택');
+  const [ regions, setRegions ] = useState<string[]>([]);
+  const cityNames = Object.keys(cityNameInfo);
+
+  const cityChangeHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const isRegions = Boolean(cityNameInfo[e.target.value]);
+    if (isRegions) {
+      setRegions(cityNameInfo[e.target.value]);
+    } else {
+      setRegions([]);
+    }
+    setCity(e.target.value);
+    setRegionName('시/군/구 선택')
+  }
+
+  const regionChangeHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setRegionName(e.target.value);
+  }
+
+  const moveButtonClickHandler = async () => {
+    const addressInfo = await getBuildingInfo(`${city} ${regionName}`);
+    setMapLoc({ lat: addressInfo.documents[0].y, lng: addressInfo.documents[0].x });
+    setIsMap(true);
+  }
+  
+  return (
+    <section className="p-3">
+      <article className="flex items-center">
+        <div>
+          <LuArrowUpRightSquare size="25" color="#4A68F5" />
+        </div>
+        <div className="text-xl text-default font-bold ml-3">지도 위치 이동</div>
+      </article>
+      <article className="grid grid-cols-2 gap-3 p-5">
+        <select
+          className="outline-none bg-white-gray px-3 py-1 rounded-lg"
+          value={city}
+          onChange={cityChangeHandler}
+        >
+          <option>시/도 선택</option>
+          {cityNames.map((cityname, idx) => (
+            <option key={idx}>{cityname}</option>
+          ))}
+        </select>
+        <select
+          className="outline-none bg-white-gray px-3 py-1 rounded-lg"
+          value={regionName}
+          onChange={regionChangeHandler}
+        >
+          <option>시/군/구 선택</option>
+          {regions.map((region, idx) => (
+            <option key={idx}>{region}</option>
+          ))}
+        </select>
+      </article>
+      <article className="px-5">
+        <button
+          className="bg-default rounded-lg text-white w-full py-3"
+          type="button"
+          onClick={moveButtonClickHandler}
+        >이동</button>
+      </article>
+    </section>
+  );
+}
+
+interface PropsType {
+  setIsMap: React.Dispatch<React.SetStateAction<boolean>>
+}
--- src/app/record/_components/RecordMain.tsx
+++ src/app/record/_components/RecordMain.tsx
@@ -2,6 +2,8 @@
 
 import { useSearchParams } from "next/navigation";
 import { useState } from "react";
+import { FaListUl } from "react-icons/fa";
+import { IoMapOutline } from "react-icons/io5";
 
 import MapSection from "../../_components/map/MapSection";
 import PanelSection from "./PanelSection";
@@ -19,15 +21,25 @@ export default function RecordMain() {
   return (
     <>
       <button
-        className="absolute top-5 right-5 bg-white border-[1.5px] border-default text-sm px-4 py-2 rounded-md z-[29] md:hidden"
+        className="absolute bottom-5 left-1/2 -translate-x-1/2 bg-default text-white text-sm px-4 py-2 rounded-full z-[29] md:hidden"
         type="button"
         onClick={isMapHandler}
-      >{isMap ? '목록으로 보기' : '지도로 보기'}</button>
+      >{isMap ? (
+        <div className="flex items-center">
+          <FaListUl size={15} color="white" />
+          <div className="ml-1">목록 보기</div>
+        </div>
+        ) : (
+          <div className="flex items-center">
+            <IoMapOutline size={15} color="white" />
+            <div className="ml-1">지도 보기</div>
+          </div>
+        )}</button>
       <MapSection />
       {sidebar ? (
         <RecordSideBar />
       ) : (!isMap && (
-        <PanelSection />
+        <PanelSection setIsMap={setIsMap} />
       ))}
     </>
   );
