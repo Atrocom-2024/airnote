0d7473141f598ba1a4f2f77986c3baca7ab21acf
TaeWooKim-SCH <zop1234@hanmail.net>
2024-08-11 02:49:03 +0900

    fix: 로딩 UI 스타일 수정

--- src/app/_components/LoadingUI.tsx
+++ src/app/_components/LoadingUI.tsx
@@ -2,7 +2,7 @@ import { AiOutlineLoading } from "react-icons/ai";
 
 export default function LoadingUI({ className }: React.ComponentProps<'main'>) {
   return (
-    <main className={`fixed top-0 left-0 w-[100vw] h-[100vh] bg-white/50 flex justify-center items-center z-50 ${className}`}>
+    <main className={`fixed top-0 left-0 w-[100vw] h-[100vh] bg-white flex justify-center items-center z-50 ${className}`}>
       <div className="flex justify-center items-center">
         <AiOutlineLoading className="animate-spin" size="50" color="#4A68F5" />
       </div>
