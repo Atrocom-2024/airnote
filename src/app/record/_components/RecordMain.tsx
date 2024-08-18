02b0bf17424daf3b720750207360b1ee85e25212
TaeWooKim-SCH <zop1234@hanmail.net>
2024-08-18 15:05:50 +0900

    feat: 사이드바 지도보기 추가

--- src/app/record/_components/RecordMain.tsx
+++ src/app/record/_components/RecordMain.tsx
@@ -4,10 +4,10 @@ import { useState } from "react";
 import { FaListUl } from "react-icons/fa";
 import { IoMapOutline } from "react-icons/io5";
 
+import { useSidebar } from "@/app/_lib/store";
 import MapSection from "../../_components/map/MapSection";
 import PanelSection from "./PanelSection";
 import RecordSideBar from "./RecordSideBar";
-import { useSidebar } from "@/app/_lib/store";
 
 export default function RecordMain() {
   const [isMap, setIsMap] = useState(false);
@@ -20,14 +20,14 @@ export default function RecordMain() {
   return (
     <>
       <button
-        className="fixed bottom-[6vh] left-1/2 -translate-x-1/2 bg-default text-white text-sm px-4 py-2 rounded-full z-[29] md:hidden"
+        className="fixed bottom-[6vh] left-1/2 -translate-x-1/2 bg-default text-white text-sm px-4 py-2 rounded-full z-[30] md:hidden"
         type="button"
         onClick={isMapHandler}
       >{isMap ? (
-        <div className="flex items-center">
-          <FaListUl size={15} color="white" />
-          <div className="ml-1">목록 보기</div>
-        </div>
+          <div className="flex items-center">
+            <FaListUl size={15} color="white" />
+            <div className="ml-1">목록 보기</div>
+          </div>
         ) : (
           <div className="flex items-center">
             <IoMapOutline size={15} color="white" />
@@ -36,7 +36,14 @@ export default function RecordMain() {
         )}</button>
       <MapSection />
       {isSidebar ? (
-        <RecordSideBar />
+        <>
+          <div className="hidden md:block">
+            <RecordSideBar />
+          </div>
+          <div className="block md:hidden">
+            {!isMap && <RecordSideBar />}
+          </div>
+        </>
       ) : (
         <>
           <div className="hidden md:block">
