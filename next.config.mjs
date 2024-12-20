2e931646747a299d7e7c3d7c2c61922445ea806e
TaeWooKim-SCH <zop1234@hanmail.net>
2024-12-20 21:09:39 +0900

    fix: s3 버킷 변경으로 인한 next.config 변경

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'prod-airnote.kr.object.ncloudstorage.com',
+        hostname: 'airnote-dev-bucket.s3.ap-northeast-2.amazonaws.com',
       },
     ],
   },
