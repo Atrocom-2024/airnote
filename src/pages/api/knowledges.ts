76ca0f0594de62e2a59b256ca730018aebd3c4ec
TaeWooKim-SCH <zop1234@hanmail.net>
2026-02-15 18:06:19 +0900

    refactor: 공간 지식 목록 데이터 페칭

--- src/pages/api/knowledges.ts
+++ src/pages/api/knowledges.ts
@@ -1,84 +1,94 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getToken } from "next-auth/jwt";
+import { ResultSetHeader, RowDataPacket } from "mysql2";
 
 import { pool } from "@/utils/database";
 import { generateRandomString } from "@/utils/modules";
 
 const secret = process.env.NEXT_AUTH_SECRET;
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  let client;
-
-  try {
-    switch (req.method) {
-      case 'GET':
-        client = await pool.connect();
-        const knowledgesQuery = `
-          SELECT
-            k.knowledge_id,
-            CASE WHEN u.nickname IS NULL THEN '(탈퇴 사용자)' ELSE u.nickname END as author_nickname,
-            k.knowledge_title,
-            k.knowledge_content,
-            SUM(CASE WHEN krt.knowledge_reaction_type = 'like' THEN 1 ELSE 0 END)::INTEGER AS likes,
-            SUM(CASE WHEN krt.knowledge_reaction_type = 'dislike' THEN 1 ELSE 0 END)::INTEGER AS dislikes,
-            k.thumbnail_url,
-            k.create_at
-          FROM KNOWLEDGE_TB k
-          LEFT JOIN USERS_TB u ON k.author_id = u.id
-          LEFT JOIN KNOWLEDGE_REACTION_TB krt ON k.knowledge_id = krt.knowledge_id
-          GROUP BY k.knowledge_id, u.nickname, k.knowledge_title, k.knowledge_content, k.thumbnail_url, k.create_at
-          ORDER BY k.create_at DESC;
-        `
-        const knowledgesQueryResult = await client.query(knowledgesQuery);
-        
-        return res.status(200).json(knowledgesQueryResult.rows);
-      case 'POST':
-        const token = await getToken({ req, secret });
-    
-        // Unauthorized
-        if (!token || !token.email) {
-          return res.status(401).send('접근 권한 없음');
-        }
-  
-        client = await pool.connect();
-        const body: BodyTypes = req.body;
-        const userCheckQuery = 'SELECT id FROM USERS_TB WHERE email = $1';
-        const userCheckResult = await client.query(userCheckQuery, [token.email]);
-        const authorId = userCheckResult.rows[0].id;
-        const knowledgeId = generateRandomString();
-
-        if (!authorId) {
-          return res.status(401).send('접근 권한 없음');
-        }
-  
-        const knowledgeInsertQuery = `
-          INSERT INTO KNOWLEDGE_TB (knowledge_id, author_id, knowledge_title, knowledge_content, thumbnail_url)
-          VALUES ($1, $2, $3, $4, $5)
-          RETURNING knowledge_id
-        `;
-        const knowledgeInsertValues = [
-          knowledgeId,
-          authorId,
-          body.knowledge_title,
-          body.knowledge_content,
-          body.thumbnail_url
-        ];
-        const knowledgeInsertResult = await client.query(knowledgeInsertQuery, knowledgeInsertValues);
-        
-        return res.status(201).json(knowledgeInsertResult.rows[0]);
-      default:
-        return res.status(405).send('잘못된 요청 메서드');
-    }
-  } catch (err) {
-    console.error(err);
-    return res.status(500).send("내부 서버 오류");
-  } finally {
-    client?.release();
-  }
-}
-
 interface BodyTypes {
   knowledge_title: string;
   knowledge_content: string;
   thumbnail_url: string;
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    switch (req.method) {
+      case 'GET':
+        return await getKnowledges(res);
+      case 'POST':
+        return await createKnowledge(req, res);
+      default:
+        return res.status(405).send('잘못된 요청 메서드');
+    }
+  } catch (err) {
+    console.error("API Error:", err);
+    return res.status(500).send("내부 서버 오류");
+  }
+}
+
+async function getKnowledges(res: NextApiResponse) {
+  const knowledgesQuery = `
+    SELECT
+      k.knowledge_id,
+      CASE WHEN u.nickname IS NULL THEN '(탈퇴 사용자)' ELSE u.nickname END as author_nickname,
+      k.knowledge_title,
+      k.knowledge_content,
+      SUM(CASE WHEN krt.knowledge_reaction_type = 'like' THEN 1 ELSE 0 END)::INTEGER AS likes,
+      SUM(CASE WHEN krt.knowledge_reaction_type = 'dislike' THEN 1 ELSE 0 END)::INTEGER AS dislikes,
+      k.thumbnail_url,
+      k.create_at
+    FROM KNOWLEDGE_TB k
+    LEFT JOIN USERS_TB u ON k.author_id = u.id
+    LEFT JOIN KNOWLEDGE_REACTION_TB krt ON k.knowledge_id = krt.knowledge_id
+    GROUP BY k.knowledge_id, u.nickname, k.knowledge_title, k.knowledge_content, k.thumbnail_url, k.create_at
+    ORDER BY k.create_at DESC;
+  `
+  const [rows] = await pool.query<RowDataPacket[]>(knowledgesQuery);
+
+  return res.status(200).json(rows);
+}
+
+async function createKnowledge(req: NextApiRequest, res: NextApiResponse) {
+  const token = await getToken({ req, secret });
+    
+  // Unauthorized
+  if (!token || !token.email) {
+    return res.status(401).send('접근 권한 없음');
+  }
+  
+  const body: BodyTypes = req.body;
+  const userCheckQuery = 'SELECT id FROM USERS_TB WHERE email = ?';
+  const [userRows] = await pool.query<RowDataPacket[]>(userCheckQuery, [token.email]);
+
+  const authorId = userRows[0].id;
+  const knowledgeId = generateRandomString();
+
+  if (!authorId) {
+    return res.status(401).send('접근 권한 없음');
+  }
+  
+  const knowledgeInsertQuery = `
+    INSERT INTO KNOWLEDGE_TB (knowledge_id, author_id, knowledge_title, knowledge_content, thumbnail_url)
+    VALUES (?, ?, ?, ?, ?)
+    RETURNING knowledge_id
+  `;
+
+  const knowledgeInsertValues = [
+    knowledgeId,
+    authorId,
+    body.knowledge_title,
+    body.knowledge_content,
+    body.thumbnail_url
+  ];
+  const [result] = await pool.query<ResultSetHeader>(knowledgeInsertQuery, knowledgeInsertValues);
+  
+  if (result.affectedRows === 1) {
+    // 성공 시 생성된 ID 반환
+    return res.status(201).json({ knowledge_id: knowledgeId });
+  } else {
+    throw new Error("게시글 생성 실패");
+  }
 }
--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,13 +1,27 @@
 import { Pool } from "pg";
 
-export const pool = new Pool({
-  user: process.env.DB_USER,
+import mysql from "mysql2/promise";
+
+export const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
-  options: `-c search_path=${process.env.DB_SCHEMA}`,
-  max: 20, // 최대 연결 수
-  idleTimeoutMillis: 30000, // 비활성 연결 대기 시간 (30초)
-  connectionTimeoutMillis: 2000, // 연결 시도 제한 시간 (2초)
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT) || 3306,
+  waitForConnections: true,
+  connectionLimit: 20, // 최대 연결 수
+  queueLimit: 0,
+  dateStrings: true, // 쿼리 결과를 날짜 객체 등으로 자동 변환
 });
+
+// export const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: Number(process.env.DB_PORT),
+//   options: `-c search_path=${process.env.DB_SCHEMA}`,
+//   max: 20, // 최대 연결 수
+//   idleTimeoutMillis: 30000, // 비활성 연결 대기 시간 (30초)
+//   connectionTimeoutMillis: 2000, // 연결 시도 제한 시간 (2초)
+// });
